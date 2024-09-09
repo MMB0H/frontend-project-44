777a662f35b2afb04510ef5e3765125fd046f292
Marianna <meiksina@gmail.com>
2024-09-09 19:50:45 +0300

    fix linter

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
-const getRandomInRange = (min = 0, max = 100) =>
+const getRandomInRange = (min = 0, max = 100) => {
   Math.floor(min + Math.random() * (max - min + 1));
+};
 
 export default getRandomInRange;
